4757c465e1aacfd987e7bb4d86ebe896db12fd38
emilfrisk <emilfrisk@10.126.33.94>
2014-05-08 11:00:12 +0200

    updated exceptionhandling

--- src/gui/FXMLDocumentController.java
+++ src/gui/FXMLDocumentController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gui;
 
 import Parser.TransactionParser;
@@ -15,6 +14,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,6 +25,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
+import storage.DatabaseFacade;
 
 /**
  * FXML Controller class
@@ -31,11 +33,12 @@ import org.xml.sax.SAXException;
  * @author emilfrisk
  */
 public class FXMLDocumentController implements Initializable {
+
     @FXML
     private Label niveauLabel;
 
     Facade facade = new Facade();
-    
+
     @FXML
     private TextField userIdInputField;
     @FXML
@@ -56,26 +59,27 @@ public class FXMLDocumentController implements Initializable {
     private Label checkExpiredPointsLabel;
     @FXML
     private Label recieveTransactionsLabel;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void displayNiveau(ActionEvent event) {
         int userId = Integer.parseInt(userIdInputField.getText());
         User user = facade.findUser(userId);
-        
+
         nameLabel.setText(user.getName());
         niveauLabel.setText(user.getLevel().toString());
         creationDateLabel.setText(user.getCreationDate().toString());
-        amountSpentLabel.setText(""+user.getAmountSpentThisYear());
+        amountSpentLabel.setText("" + user.getAmountSpentThisYear());
         emailLabel.setText(user.getEmail());
         addressLabel.setText(user.getAddress());
-        
+
     }
 
     @FXML
@@ -85,25 +89,27 @@ public class FXMLDocumentController implements Initializable {
     }
 
     @FXML
-    private void recieveTransactionsAction(ActionEvent event) throws ParserConfigurationException, SAXException, IOException {
+    private void recieveTransactionsAction(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         File file = fileChooser.showOpenDialog(null);
 
         FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TXT Files (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(filter);
-        
+
         TransactionParser transactionParser = new TransactionParser();
-        
+
         List<Transaction> transactionList = null;
         try {
             transactionList = transactionParser.readFile(file);
-        } catch (Exception e) {
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            Logger lgr = Logger.getLogger(FXMLDocumentController.class.getName());
+            lgr.log(Level.SEVERE, e.getMessage(), e);
         }
-        
+
         facade.recieveTransactions(transactionList);
-        
+
         recieveTransactionsLabel.setText("Transactions processed!");
     }
-    
+
 }
